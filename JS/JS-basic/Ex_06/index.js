9f46d81e0a7bdd47c024f8070b31f33d7e1fbb91
Apolo <whoami.apolo@gmail.com>
2021-09-05 00:21:56 -0300

    entender variáveis não inicializadas

---
+++ JS/JS-basic/Ex_06/index.js
@@ -0,0 +1,20 @@
+var a
+a = 5
+var b 
+b = 10
+var c 
+c = "I am a "
+
+a += 1;
+b += 5;
+c += "String";
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+
+console.log(
+"Valor de A será:" , a +
+"\n" + "Valor de B será:" , b +
+"\n" + "Valor de C será:" , c
+);
