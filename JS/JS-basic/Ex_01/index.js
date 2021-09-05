81bbe621f4b299f7bdac7e15aa15641c87c62b8f
Apolo <whoami.apolo@gmail.com>
2021-09-04 23:09:30 -0300

    comente o seu código JS

---
+++ JS/JS-basic/Ex_01/index.js
@@ -0,0 +1,7 @@
+var currentLine = "Comentará apenas a linha atual. Veja o exemplo a seguir:"
+var comment = currentLine
+console.log('1° opção -', comment, "\n//Comentará apenas a linha atual")
+
+var multiLine = "Comentará multiplas linhas. Veja o exemplo a seguir:"
+var comment = multiLine
+console.log('2° alternativa -', comment, "\n/*\nColocou aqui, virou comentário\n*/") 
