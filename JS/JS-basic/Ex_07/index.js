91cd63c9a39c5c3010e4a98d5a6c870ab570b972
Apolo <whoami.apolo@gmail.com>
2021-09-05 00:40:04 -0300

    entender a sensibilidade a caracteres maiúsculos e minúsculos em variáveis

---
+++ JS/JS-basic/Ex_07/index.js
@@ -0,0 +1,8 @@
+var someVariable
+var anothehrVariableName
+var ThisVariableNameIsSoLong
+
+var exemploDeCamelCase
+exemploDeCamelCase = "Primeira letra de cada palavra subsequente terá a letra maiúscula\nPor exemplo: var: esteExemplo"
+
+console.log(exemploDeCamelCase)
