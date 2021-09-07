266a2718e4d3713aa1a9c53d1d93d57090d56140
Apolo <whoami.apolo@gmail.com>
2021-09-07 19:34:37 -0300

    Escapar sequências em strings

---
+++ JS/JS-basic/Ex_25/index.js
@@ -0,0 +1,28 @@
+let exemplos, codigo, saida
+codigo = "Código:"
+saida = "Saída:"
+
+exemplos = "\\'"
+console.log(codigo + "\t\t" + saida + "\n" + exemplos + "\t\t" + " Aspas simples")
+
+exemplos = '\\"'
+console.log(exemplos + "\t\t" + " Aspas duplas")
+
+exemplos = "\\"
+console.log(exemplos + "\\"+ "\t\t" + " Barra invertida")
+
+exemplos = "\\n"
+console.log(exemplos + "\t\t" + " Nova linha")
+
+exemplos = "\\r"
+console.log(exemplos + "\t\t" + " Retorno de carro")
+
+exemplos = "\\t"
+console.log(exemplos + "\t\t" + " Tab")
+
+exemplos = "\\b"
+console.log(exemplos + "\t\t" + " Limite de palavra")
+
+exemplos = "\\f"
+console.log(exemplos + "\t\t" + " Quebra de página")
+
