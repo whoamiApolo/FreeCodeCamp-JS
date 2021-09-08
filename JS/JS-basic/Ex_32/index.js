4e0e390b75bf40902c976bdb877576355692dc96
Apolo <whoami.apolo@gmail.com>
2021-09-07 21:49:22 -0300

    entender a imutabilidade das strings

---
+++ JS/JS-basic/Ex_32/index.js
@@ -0,0 +1,11 @@
+let xyz = "Jello World"
+console.log(xyz)
+
+/* 
+xyz[0] = "HelloWorld" 
+- String são imutáveis, o que significa que elas não podem ser alteradas após serem criadas.
+Por isso, devemos alterá-la atribuindo a ela uma nova string, dessa forma:
+*/
+
+xyz = "HelloWorld"
+console.log(xyz)
