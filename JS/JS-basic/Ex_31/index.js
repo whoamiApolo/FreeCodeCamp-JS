840478c4da9407ee4753a0a7fb726b2f861a65d8
Apolo <whoami.apolo@gmail.com>
2021-09-07 21:36:47 -0300

    usar notação de colchetes para encontrar o 'primeiro' caractere em uma string

---
+++ JS/JS-basic/Ex_31/index.js
@@ -0,0 +1,15 @@
+let firstLetterOfLastName, lastName
+firstLetterOfLastName = ""
+lastName = "Lovelace"
+
+firstLetterOfName = lastName
+console.log(
+firstLetterOfName[0], 
+firstLetterOfName[1],
+firstLetterOfName[2],
+firstLetterOfName[3],
+firstLetterOfName[4],
+firstLetterOfName[5],
+firstLetterOfName[6],
+firstLetterOfName[7],
+)
