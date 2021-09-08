b7d3b833259b9d8d377ce5877c0b64c927ba68b7
Apolo <whoami.apolo@gmail.com>
2021-09-07 21:22:01 -0300

    encontrar o tamanho de uma string

---
+++ JS/JS-basic/Ex_30/index.js
@@ -0,0 +1,12 @@
+let firstNameLength, firstName, lastNameLength, lastName
+firstNameLength = "0"
+firstName = "Ada"
+
+firstNameLength = firstName.length
+console.log(firstNameLength)
+
+lastNameLength = "0"
+lastName = "Lovelace"
+
+lastNameLength = lastName.length
+console.log(lastNameLength)
