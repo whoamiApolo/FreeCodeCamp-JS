86737f7a36a919afe3f2f78d68ac60d312e818e3
Apolo <whoami.apolo@gmail.com>
2021-09-11 23:22:22 -0300

    preencher espaços em branco

---
+++ JS/JS-basic/Ex_36/index.js
@@ -0,0 +1,10 @@
+let  myNoun, myAdjective, myVerb, myAdverb, wordBlanks
+myNoun = "dog"
+myAdjective = "big"
+myVerb = "ran"
+myAdverb = "quickly"
+
+wordBlanks = "My " + myNoun + " he's " + myAdjective + ".\nHe " + myVerb + " and " + myAdverb + " ate his ration."
+
+console.log(wordBlanks)
+console.log("Translation performed by Google Tradutor.")
