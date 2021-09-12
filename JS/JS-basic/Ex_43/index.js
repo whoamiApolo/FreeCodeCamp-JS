c6deb9976257855dbe03c4b3d126d74f64174235
Apolo <whoami.apolo@gmail.com>
2021-09-12 16:59:14 -0300

    manipular arrays com pop()

---
+++ JS/JS-basic/Ex_43/index.js
@@ -0,0 +1,16 @@
+let myArray, array, removedFromMyArray
+
+myArray = [["John", 23], ["cat", 2]]
+removedFromMyArray = myArray.pop()
+
+/* 
+["cat", 2]
+*/
+
+console.log(removedFromMyArray) 
+
+/* 
+["John", 23]
+*/
+
+console.log(myArray)
