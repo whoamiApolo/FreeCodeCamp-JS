089ff2231634bf12f74427c4affd97b9142a5967
Apolo <whoami.apolo@gmail.com>
2021-09-12 17:47:31 -0300

    manipular arrays com unshift

---
+++ JS/JS-basic/Ex_45/index.js
@@ -0,0 +1,15 @@
+let myArray = [["John", 23], ["dog", 3]]
+
+/* 
+["dog", 3]
+*/
+
+myArray.shift()
+console.log(myArray)
+
+/* 
+.unshift() funciona exatamente como .push(), mas, ao invés de adicionar o elemento ao final do array, unshift() adiciona o elemento no início do array.
+*/
+
+myArray.unshift(["Paul", 35])
+console.log(myArray)
