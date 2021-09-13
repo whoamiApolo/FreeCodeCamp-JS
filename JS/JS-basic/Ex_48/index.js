5da1bfbf67bae7b8a698b0ec76f7c3a1b7c4d89b
Apolo <whoami.apolo@gmail.com>
2021-09-13 00:19:50 -0300

    passar valores para funções com argumentos

---
+++ JS/JS-basic/Ex_48/index.js
@@ -0,0 +1,13 @@
+/* 
+Aqui está uma função com dois parâmetros, param1 e param2:
+*/
+function functionWithArgs(para1, para2) {
+    console.log(para1 + para2) 
+}
+
+/* 
+Eis a questão criar outra função ou usá-la novamente?
+*/
+
+functionWithArgs(1, 2)
+functionWithArgs(7, 9)
