a507a4b061d20f76a1ef3b3d73123a725a7317b8
Apolo <whoami.apolo@gmail.com>
2021-09-14 22:18:36 -0300

    retornar um valor de uma função com return

---
+++ JS/JS-basic/Ex_52/index.js
@@ -0,0 +1,16 @@
+function timesFive(x) {
+
+/* 
+Utilizando uma instrução return para enviar um valor para fora de uma função.
+*/
+
+  return x * 5
+}
+
+const result = timesFive(5)
+const resultOne = timesFive(0)
+const resultSecond = timesFive(2)
+
+console.log(result)
+console.log(resultOne)
+console.log(resultSecond)
