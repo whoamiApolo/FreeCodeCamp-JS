a5eaede16f083fd2d2e8b8bc58c9dc8b0ebd4fab
Apolo <whoami.apolo@gmail.com>
2021-09-18 18:23:45 -0300

    usar lógica condicional com instruções if

---
+++ JS/JS-basic/Ex_57/index.js
@@ -0,0 +1,9 @@
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+  }
+  
+  console.log(trueOrFalse(true)) //Yes, that was true
+  console.log(trueOrFalse()) // false
