6b682a13d36cbf9d2f68a68aa7d5cd757c0a7d1b
Apolo <whoami.apolo@gmail.com>
2021-09-23 22:19:52 -0300

    usar a ordem lógica em instruções if else

---
+++ JS/JS-basic/Ex_71/index.js
@@ -0,0 +1,10 @@
+function orderMyLogic(val) {
+    if(val < 5) {
+        return "Less than 5"
+    } else if (val < 10) {
+        return "Less than 10"
+    }
+    return "Greater than or equal to 10"
+}
+
+console.log(orderMyLogic(7))
