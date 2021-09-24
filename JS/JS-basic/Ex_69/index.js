568d3332ede33420a08bbd5f8f64f0b9048493d6
Apolo <whoami.apolo@gmail.com>
2021-09-23 22:02:44 -0300

    introduzir instruções else

---
+++ JS/JS-basic/Ex_69/index.js
@@ -0,0 +1,12 @@
+function testElse(val) {
+    var result = ""
+
+    if(val > 5) {
+        result = "Bigger than 5"
+    } else {
+        result = "5 or smaller"
+    }
+    return result
+}
+
+console.log(testElse(4))
