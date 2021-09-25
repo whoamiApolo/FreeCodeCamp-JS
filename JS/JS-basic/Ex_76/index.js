f1120aa5925ef737feb52bd4ef7595c62eef0d5a
Apolo <whoami.apolo@gmail.com>
2021-09-25 17:10:18 -0300

    lidar com várias opções idênticas em instruções switch

---
+++ JS/JS-basic/Ex_76/index.js
@@ -0,0 +1,34 @@
+function sequentialSizes(val) {
+    var answer = ""
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+        answer = "Low"
+        break
+
+        case 4:
+        case 5:
+        case 6:
+        answer = "Mid"
+        break
+
+        case 7:
+        case 8:
+        case 9:
+        answer = "High"
+        break
+    }
+
+    return answer
+}
+
+console.log(sequentialSizes(1))
+console.log(sequentialSizes(2))
+console.log(sequentialSizes(3))
+console.log(sequentialSizes(4))
+console.log(sequentialSizes(5))
+console.log(sequentialSizes(6))
+console.log(sequentialSizes(7))
+console.log(sequentialSizes(8))
+console.log(sequentialSizes(9))
