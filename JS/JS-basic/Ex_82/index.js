65c1fd4729b6850164b96d143bdc85d0b2183b56
Apolo <whoami.apolo@gmail.com>
2021-10-02 13:10:01 -0300

    acessar propriedades de objetos com notação de pontos

---
+++ JS/JS-basic/Ex_82/index.js
@@ -0,0 +1,10 @@
+let testObj = {
+    "hat": "ballacap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+}
+
+let hatValue = testObj.hat
+let shirtValue = testObj.shirt
+
+console.log(hatValue + "\n" + shirtValue)
