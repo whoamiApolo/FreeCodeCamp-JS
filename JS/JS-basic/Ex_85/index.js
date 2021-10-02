550d896baaea674a2110f3e456d9369cb721ebab
Apolo <whoami.apolo@gmail.com>
2021-10-02 14:04:37 -0300

    atualizar propriedades do objeto

---
+++ JS/JS-basic/Ex_85/index.js
@@ -0,0 +1,14 @@
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tais": 1,
+    "friends": ["freeCodeCamp Campers"]
+}
+
+/* 
+myDog[name] = "Happy Coder"
+*/
+
+myDog.name = "Happy Coder"
+
+console.log(myDog)
