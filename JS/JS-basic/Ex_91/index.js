6a745a68de133124f6d35bb14fafa78223bb5fe0
Apolo <whoami.apolo@gmail.com>
2021-10-02 22:04:02 -0300

    acessar objetos aninhados

---
+++ JS/JS-basic/Ex_91/index.js
@@ -0,0 +1,16 @@
+var myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  var gloveBoxContents = myStorage.car.inside["glove box"];
+
+
+  console.log(gloveBoxContents)
