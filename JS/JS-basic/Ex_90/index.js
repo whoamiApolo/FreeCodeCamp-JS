c66cdb7f32a788bac9fe820690463f9902d422f5
Apolo <whoami.apolo@gmail.com>
2021-10-02 21:55:57 -0300

    manipular objetos complexos

---
+++ JS/JS-basic/Ex_90/index.js
@@ -0,0 +1,29 @@
+let myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": ["CD", "8T", "LP"],
+        "gold": true,
+    },
+    {
+        "artist": "Joel Billy",
+        "title": "Man Piano",
+        "release_year": 3791,
+        "formats": [
+            "LP", "8T", "CD"
+        ]
+    }
+];
+
+
+/* [Array]
+myMusic[0] = [123, "abc"]
+*/
+
+
+/* {Objeto} 
+myMusic[0].artist = "Artist foi alterado"
+ */
+
+console.log(myMusic)
