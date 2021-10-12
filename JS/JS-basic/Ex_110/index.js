e85a6d4a71f8445c1a18fa1d176d2843a417a389
Apolo <whoami.apolo@gmail.com>
2021-10-12 19:56:27 -0300

    usar recursão ppara criar uma contagem regressiva

--- JS/JS-basic/Ex_110/index.js
+++ JS/JS-basic/Ex_110/index.js
@@ -1,19 +1,18 @@
-function countDown(n) {
+function countdown(n) {
 
     if (n < 1) {
         return []
     } else {
-        const countArray = countDown(n - 1)
-        countArray.unshift(n) // [5, 4, 3, 2, 1]
-        /*countArray.push(n)  [1, 2, 3, 4, 5]*/
+        const countArray = countdown(n - 1)
+        countArray.unshift(n) // [5, 4, 3, 2, 1] || countArray.push(n)  [1, 2, 3, 4, 5]
         return countArray
     }
 }
 
-console.log(countDown(5))
+console.log(countdown(5))
 
 /* 
-Outras formas de resoler o exercicio:
+Outras formas de resoler o exercício:
 Opção 1:
    return n < 1 ? [] : [n, ...countdown(n - 1)];
 Opção 2:
